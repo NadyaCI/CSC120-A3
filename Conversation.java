381fc21895c01d0f60fc11b0e03f7291755f63a3
NadyaCI <144396008+NadyaCI@users.noreply.github.com>
2023-10-02 02:01:27 +0000

    first draft

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,18 @@
-class Conversation {
+import java.util.Scanner;
 
+class Conversation {
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    int rounds = 0;
+    String sentence = "";
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How many rounds of conversation would you like?");
+    rounds = input.nextInt();
+    //System.out.println("hi" + rounds);
+    for (int i = 0; i<rounds; i++){
+      System.out.println("What would you like to talk about?");
+      sentence = input.nextLine();
+
+    }
   }
 }
